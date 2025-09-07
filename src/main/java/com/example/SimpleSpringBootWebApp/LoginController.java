22fece96ce1d4335303864302d94847cfe199718
Garima <2412garima@gmail.com>
2025-09-07 22:19:52 +0530

    multiple controllers for different purpose

---
+++ src/main/java/com/example/SimpleSpringBootWebApp/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.SimpleSpringBootWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String greet()
+    {
+        return "Login page";
+    }
+
+}
