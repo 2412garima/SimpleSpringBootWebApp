cad4b5278bc24f9b235d1d8219282c5ed820ce6e
Garima <2412garima@gmail.com>
2025-09-07 21:38:32 +0530

    simple springboot web app

---
+++ src/main/java/com/example/SimpleSpringBootWebApp/SimpleSpringBootWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.SimpleSpringBootWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SimpleSpringBootWebAppApplication.class, args);
+	}
+
+}
